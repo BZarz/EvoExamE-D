1ab8b4e22a45fcee63b55b960244d9bcb42a297c
BZarz <yunizar554@gmail.com>
2025-05-13 14:16:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const cipherMap = {
     'f': 'hk5', 'g': 'r6v', 'h': 'o7z', 'i': 'y8p', 'j': 't9e',
     'k': 'v10l', 'l': 'jg', 'm': 'lk', 'n': 'er', 'o': 'vg',
     'p': 'ok', 'q': 'pl', 'r': 'qw', 's': 'fr', 't': 'sc',
-    'u': 'sh', 'v': 'bt', 'w': 'ml', 'x': 'ox', 'y': 'bk', 'z': 'io',
-    ':': 'bdj', '/': 'blk',
+    'u': 'sh', 'v': 'bt', 'w': 'ml', 'x': 'ox', 'y': 'bk', 'z': 'io', ':': 'bdj', '/': 'blk',
     
     'A': 'B0E', 'B': 'C1B', 'C': 'P2K', 'D': 'M3K', 'E': '4LF',
     'F': 'HK5', 'G': 'R6V', 'H': 'O7Z', 'I': 'Y8P', 'J': 'T9E',
